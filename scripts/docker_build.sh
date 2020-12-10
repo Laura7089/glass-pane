ae7b0a9ced186a94ed52ac836c867985138b589f
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2020-12-10 20:28:17 +0100

    Cleanup scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-image_name="$DOCKER_IMAGE"
-
-if [[ $(git rev-parse --abbrev-ref HEAD) -eq "develop" ]]; then
-    image_name = "$image_name:develop"
-fi
-
-docker build -t "$image_name" .
+docker build -t "$DOCKER_IMAGE" .
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-image_name="$DOCKER_IMAGE"
-username="$DOCKER_USERNAME"
-password="$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"
 
-echo "$password" | docker login -u "$username"
-
-if [[ $(git rev-parse --abbrev-ref HEAD) -eq "develop" ]]; then
-    image_name = "$image_name:develop"
-fi
-
-docker push "$image_name"
+docker push "$DOCKER_IMAGE"
